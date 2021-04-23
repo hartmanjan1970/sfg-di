33fe0a2c27d92a3bd805691afd26df8d4478cd30
Jan Hartman <janhartman@eleven.nl>
2021-04-23 14:24:01 +0200

    end assignment

---
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Jan Hartman
+ */
+@Controller
+public class PetController {
+
+	private final PetService petService;
+
+	public PetController(PetService petService) {
+		this.petService = petService;
+	}
+
+	public String whichPetIsTheBest(){
+		return petService.getPetType();
+	}
+}
